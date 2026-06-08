7ae6aa36960c8d244f4c594f483f3c0e6dc25bca
investigatorebasha-gif <investigatorebasha@gmail.com>
2026-06-08 19:13:16 +0200

    Document auth provider configuration

--- public/auth-config.js
+++ public/auth-config.js
@@ -1,5 +1,10 @@
 window.MUSEO_AUTH_CONFIG = {
+  // Google OAuth 2.0 Web Client ID, es: 1234567890-abc.apps.googleusercontent.com
   googleClientId: "",
+
+  // Apple Services ID, es: eu.itcslazzari.museosabbie.web
   appleClientId: "",
-  appleRedirectURI: "",
+
+  // Deve essere registrato tra i Return URLs di Sign in with Apple.
+  appleRedirectURI: "https://museo-sabbie-lazzari2026.github.io/5B-SIA/museo/login.html",
 };
